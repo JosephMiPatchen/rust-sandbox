c5d299084b4ca5db3ee25862ae4ec0a5e4345cff
Joseph Patchen <josephmipatchen@gmail.com>
2025-12-05 20:51:16 -0800

    docs: more :wq

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -2,6 +2,15 @@ mod fib;
 use fib::fib;
 
 fn main() {
+
+    fn bad() -> i32 {
+        let x = 5;
+        x
+    }
+
+    let result = bad();
+    println!("bad() = {}", result);
+
     let result = fib(10);
     println!("fib(10) = {}", result);
 }
